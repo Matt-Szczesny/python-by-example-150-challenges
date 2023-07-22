d135f9906d01566f1e089ef2ad16ec1406bc5751
avidi <avidi@DESKTOP-KALI.home>
2023-07-22 10:13:18 +0200

    Adding solution for excercise 64

---
+++ 064.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.pensize(2)
+turtle.pencolor("blue")
+
+for _ in range(1,6):
+    turtle.right(72)
+    turtle.forward(150)
+    turtle.right(144)
+    turtle.forward(150)
+    # turtle.right(72)
+
+turtle.exitonclick()
