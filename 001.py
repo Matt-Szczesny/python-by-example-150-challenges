1f4b5cc25856c845e8e32f4db54088451f656a92
avidi <avidi@DESKTOP-KALI.home>
2023-07-24 21:21:13 +0200

    Solution for excercise 1

---
+++ 001.py
@@ -0,0 +1,10 @@
+name = input("What's your name? ")
+
+print("Hello", name)
+
+# Alternatives: 
+# print(f"Hello {name}")
+# print("Hello %s" % name)
+# print("Hello %s" %(name))
+# print("Hello {}".format(name))
+# print("Hello {0:s}".format(name))
